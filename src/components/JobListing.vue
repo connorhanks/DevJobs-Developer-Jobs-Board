7aa360861b903512efe003f12970f91b44220783
connorhanks <hello@connorhanks.com>
2024-12-11 14:06:35 +0000

    complete home page

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -53,7 +53,7 @@ const truncatedDescription = computed(() => {
 
       <div class="flex flex-col lg:flex-row justify-between mb-4">
         <div class="text-orange-700 mb-3">
-          <span class="mdi mdi-map-marker text-green-500 mr-2"></span>
+          <span class="mdi mdi-map-marker text-orange-500 mr-2"></span>
           {{ job.location }}
         </div>
         <a
